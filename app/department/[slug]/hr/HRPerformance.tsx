5a4ba31304d9ba546c44bd0c224d4573e949900e
Khuram Saleem <khuram1901@gmail.com>
2026-09-04 18:35:13 +0500

    fix: restore # header on dept tables in HR performance view

--- app/department/[slug]/hr/HRPerformance.tsx
+++ app/department/[slug]/hr/HRPerformance.tsx
@@ -478,7 +478,7 @@ function CompanyView({
           <div style={{ overflowX: "auto" }}>
             <table style={{ width: "100%", borderCollapse: "collapse", fontSize: "13px" }}>
               <thead><tr>
-                <th style={thS}>Code</th><th style={thS}>Department</th>
+                <th style={thS}>#</th><th style={thS}>Department</th>
                 <th style={{ ...thS, textAlign: "right" }}>Tasks</th>
                 <th style={{ ...thS, textAlign: "right" }}>On time</th>
                 <th style={{ ...thS, textAlign: "right" }}>Overdue</th>
